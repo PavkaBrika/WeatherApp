e0c0880354e1aecbdc456d5eb9a29ef635a79b5d
PavkaBrika <pavlikbrichkin@mail.ru>
2022-08-07 22:48:00 +0500

    added loading textview, added loading weather in last checked city when opening app

--- app/src/main/java/com/breckneck/weatherappca/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/breckneck/weatherappca/presentation/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.breckneck.weatherappca.presentation.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,7 +14,6 @@ import com.breckneck.weatherappca.usecase.GetWeatherUseCase
 import com.breckneck.weatherappca.usecase.SaveWeatherUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.koin.android.ext.android.inject
 import org.koin.androidx.scope.activityScope
 
@@ -37,27 +37,49 @@ class MainActivity : AppCompatActivity() {
         val degreesTextView: TextView = findViewById(R.id.degreesTextView)
         val feelsLikeTextView: TextView = findViewById(R.id.feelslikeTextView)
         val weatherTextView: TextView = findViewById(R.id.weatherTextView)
+        val loadingTextView: TextView = findViewById(R.id.loadingTextView)
 
         val sp = applicationContext.getSharedPreferences(SP, MODE_PRIVATE)
         var isOpened = sp.getBoolean(open, false)
 
         if (isOpened == true) {
             activityScope().lifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
-                val weather = getWeatherDataBaseUseCase.execute()
+                val weatherDatabase = getWeatherDataBaseUseCase.execute()
                 launch(Dispatchers.Main) {
-                    cityTextView.text = "${resources.getString(R.string.city)} ${weather.city}"
-                    degreesTextView.text = "${resources.getString(R.string.temperature)} ${weather.degrees} ${resources.getString(R.string.metric)}"
-                    feelsLikeTextView.text = "${resources.getString(R.string.feelslike)} ${weather.feelsLike} ${resources.getString(R.string.metric)}"
-                    weatherTextView.text = "${resources.getString(R.string.weather)} ${weather.weather}"
+                    cityTextView.text = "${resources.getString(R.string.city)} ${weatherDatabase.city}"
+                    degreesTextView.text = "${resources.getString(R.string.temperature)} ${weatherDatabase.degrees} ${resources.getString(R.string.metric)}"
+                    feelsLikeTextView.text = "${resources.getString(R.string.feelslike)} ${weatherDatabase.feelsLike} ${resources.getString(R.string.metric)}"
+                    weatherTextView.text = "${resources.getString(R.string.weather)} ${weatherDatabase.weather}"
+                    loadingTextView.visibility = View.VISIBLE
+                }
+                val weatherRemote = getWeatherUseCase.execute(weatherDatabase.city)
+                if (weatherRemote.data != null) {
+                    saveWeatherUseCase.execute(weather = weatherRemote.data!!)
+                    sp.edit().putBoolean(open, true).apply()
+                    Log.e("TAG", "Success")
+                    Log.e("TAG", weatherRemote.data!!.city)
+                    Log.e("TAG", weatherRemote.data!!.weather)
+                    Log.e("TAG", weatherRemote.data!!.degrees.toString())
+                    Log.e("TAG", weatherRemote.data!!.feelsLike.toString())
+                    launch(Dispatchers.Main) {
+                        cityTextView.text = "${resources.getString(R.string.city)} ${weatherRemote.data!!.city}"
+                        degreesTextView.text = "${resources.getString(R.string.temperature)} ${weatherRemote.data!!.degrees} ${resources.getString(R.string.metric)}"
+                        feelsLikeTextView.text = "${resources.getString(R.string.feelslike)} ${weatherRemote.data!!.feelsLike} ${resources.getString(R.string.metric)}"
+                        weatherTextView.text = "${resources.getString(R.string.weather)} ${weatherRemote.data!!.weather}"
+                        loadingTextView.visibility = View.INVISIBLE
+                    }
+                } else {
+                    loadingTextView.text = weatherRemote.message
+                    cityTextView.text = ""
+                    degreesTextView.text = ""
+                    feelsLikeTextView.text = ""
+                    weatherTextView.text = ""
                 }
             }
         }
 
-
-
-
-
         buttonSetCity.setOnClickListener {
+            loadingTextView.visibility = View.VISIBLE
             activityScope().lifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
                 val weather = getWeatherUseCase.execute(cityEditText.text.toString())
                 if (weather.data != null) {
@@ -73,21 +95,16 @@ class MainActivity : AppCompatActivity() {
                         degreesTextView.text = "${resources.getString(R.string.temperature)} ${weather.data!!.degrees} ${resources.getString(R.string.metric)}"
                         feelsLikeTextView.text = "${resources.getString(R.string.feelslike)} ${weather.data!!.feelsLike} ${resources.getString(R.string.metric)}"
                         weatherTextView.text = "${resources.getString(R.string.weather)} ${weather.data!!.weather}"
+                        loadingTextView.visibility = View.INVISIBLE
                     }
                 } else {
-                    cityTextView.text = weather.message
+                    loadingTextView.text = weather.message
+                    cityTextView.text = ""
                     degreesTextView.text = ""
                     feelsLikeTextView.text = ""
                     weatherTextView.text = ""
                 }
-
             }
-//            val weather = getWeatherUseCase.execute("eaf")
-//            Log.e("TAG", "Success")
-//            Log.e("TAG", weather.city)
-//            Log.e("TAG", weather.weather)
-//            Log.e("TAG", weather.degrees.toString())
-//            Log.e("TAG", weather.feelsLike.toString())
         }
     }
 }
